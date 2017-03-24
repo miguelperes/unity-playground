d0a690a25278a0b589d710595218953a58889652
Miguel Péres <foxmpc@gmail.com>
2017-03-24 00:31:33 -0300

    Update Tank prefab, scene and StateController

--- Live Training/TanksPluggableAI/Assets/PluggableAI/Scripts/StateController.cs
+++ Live Training/TanksPluggableAI/Assets/PluggableAI/Scripts/StateController.cs
@@ -14,6 +14,7 @@ public class StateController : MonoBehaviour {
 	[HideInInspector] public Complete.TankShooting tankShooting;
 	[HideInInspector] public List<Transform> wayPointList;
 	[HideInInspector] public int nextWayPoint;
+	[HideInInspector] public Transform chaseTarget;
 
 	private bool aiActive;
 
